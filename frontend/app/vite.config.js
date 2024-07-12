6cff8912fbbc6950a10fd14bdb1c7aa7a7f7f253
Henry Morris <hrmorri5@ncsu.edu>
2024-07-12 17:04:40 -0400

    Changed SW update stratetgy to auto update

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugIn = {
-  registerType:"prompt",
+  registerType:"autoUpdate",
   includeAssests:["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest:{
       name:"Recipes",
